8fe0f32cdab7711583f740d2cfe181cfa5ca74f8
Sergei A. Trusov <sergei.a.trusov@ya.ru>
2021-06-05 17:47:02 +1000

    Диалект назван Рефал-М (версия 0.1.4 альфа).

--- src/main.c
+++ src/main.c
@@ -9,8 +9,8 @@
 #include "interpreter.h"
 #include "translator.h"
 
-#define REFAL_NAME "РЕФАЛ-машина"
-#define REFAL_VERSION "версия 0.1.3 (альфа)"
+#define REFAL_NAME "Рефал-М"
+#define REFAL_VERSION "версия 0.1.4 (альфа)"
 
 #define REFAL_INITIAL_MEMORY      (128*1024/sizeof(rf_cell))
 #define REFAL_TRIE_INITIAL_MEMORY (128*1024/sizeof(struct rtrie_node))

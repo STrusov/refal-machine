8c382bd028a4a8d6106fb694b6523b5099332154
Sergei A. Trusov <sergei.a.trusov@ya.ru>
2024-04-12 21:33:44 +1000

    rf_output: добавлен пропущенный break (ветка rf_atom не используется).

--- src/library.c
+++ src/library.c
@@ -128,6 +128,7 @@ int rf_output(
                  ? RF_COLOR_SYMBOL" %s"RF_ESC_RESET
                  : RF_COLOR_SYMBOL"%s"RF_ESC_RESET,
                  vm->u[i].atom);
+         break;
       case rf_identifier:
          fprintf(stream, prevt == rf_identifier
                  ? RF_COLOR_SYMBOL" #%x"RF_ESC_RESET
